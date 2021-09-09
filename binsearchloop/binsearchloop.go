a2e314ac7b958d06db502786b81f22061fee8a0a
DymaSv <dymasvyrydonov@gmail.com>
2021-09-08 21:50:40 -0400

    Added algorithm that use loop for Binary Search

--- binsearchloop/binsearchloop.go
+++ binsearchloop/binsearchloop.go
@@ -1,8 +1,41 @@
 package binsearchloop
 
-import "fmt"
-
 //Binary search Algorithm wich use a loop
-func BinSearchLoop(array *[]int, searchValue int) {
-	fmt.Println(searchValue)
+func BinSearchLoop(array *[]int, searchValue int) int {
+	index := len(*array) / 2
+	low := 0
+	hight := 0
+	searchOneByOne := false
+	arr := *array
+	for index > 0 {
+		if index == 1 {
+			if arr[index] == searchValue {
+				return index
+			}
+			index = 0
+			searchOneByOne = true
+		}
+		if !searchOneByOne {
+			if arr[index] == searchValue {
+				return index
+			} else if arr[index] > searchValue {
+				//to left side of array
+				low = 0
+				hight = index
+			} else if arr[index] < searchValue {
+				//to right side of array
+				low = index
+				hight = len(*array)
+			}
+			index = index / 2
+		} else {
+			for i := low; i < hight; i++ {
+				if arr[i] == searchValue {
+					return i
+				}
+			}
+		}
+
+	}
+	return -1
 }
