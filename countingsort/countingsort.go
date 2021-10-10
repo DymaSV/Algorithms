44db1e405295c319b4cee74155e4e79219bd3c01
DymaSv <dymasvyrydonov@gmail.com>
2021-10-09 20:03:59 -0400

    Counting sort for positive numbers

--- countingsort/countingsort.go
+++ countingsort/countingsort.go
@@ -1,29 +1,42 @@
 package countingsort
 
-import "fmt"
-
-func CountSort(arr []int) {
-	// fmt.Println(getMap(arr))
-	fmt.Println(prefixSum(getMap(arr)))
+func CountingSort(arr []int) []int {
+	return getSortedArray(prefixSum(getMap(arr)), arr)
 }
 
-func getMap(arr []int) map[int]int {
+func getMap(arr []int) (map[int]int, int) {
 	var arrCount = make(map[int]int)
+	max := 0
 	for _, v := range arr {
+		if v > max {
+			max = v
+		}
 		arrCount[v] += 1
 	}
+	return arrCount, max
+}
+
+func prefixSum(m map[int]int, max int) map[int]int {
+	var arrCount = make(map[int]int)
+	for i := 0; i <= max; i++ {
+		if i != 0 {
+			arrCount[i] = arrCount[i-1] + m[i]
+		} else {
+			arrCount[i] = m[i]
+		}
+	}
+	for i := max; i >= 0; i-- {
+		arrCount[i] = arrCount[i-1]
+	}
+	arrCount[0] = 0
 	return arrCount
 }
 
-func prefixSum(m map[int]int) map[int]int {
-	fmt.Println(m)
-	ele := 0
-	for i, v := range m {
-		if i != 0 {
-			m[i] = ele + m[i]
-		}
-		ele = v
+func getSortedArray(m map[int]int, arr []int) []int {
+	var result = make([]int, len(arr))
+	for _, v := range arr {
+		result[m[v]] = v
+		m[v] = m[v] + 1
 	}
-	fmt.Println(m)
-	return m
+	return result
 }
--- countingsort/countingsort_test.go
+++ countingsort/countingsort_test.go
@@ -4,13 +4,47 @@ import (
 	"testing"
 )
 
-// TestDFS calls depthfirstsearch.DFS with a tree.
-//Try to find last value
-func TestCountSort(t *testing.T) {
-	// want := 7
-	x := []int{5, 3, 5, 4, 1}
-	CountSort(x)
-	// if want != result {
-	// 	t.Fatalf(`Error %d`, result.Val)
-	// }
+// TestCountingSort calls countingsort.CountingSort.
+func TestCountingSort_WithDuplicates(t *testing.T) {
+	want := []int{0, 1, 2, 3, 3, 5, 5}
+	x := []int{3, 5, 0, 1, 5, 2, 3}
+	result := CountingSort(x)
+	for i, v := range want {
+		if v != result[i] {
+			t.Fatalf(`Error %d`, result[i])
+		}
+	}
+}
+
+func TestCountingSort_WithOutDuplicates(t *testing.T) {
+	want := []int{0, 1, 2, 3, 5, 12, 16}
+	x := []int{3, 5, 0, 1, 12, 2, 16}
+	result := CountingSort(x)
+	for i, v := range want {
+		if v != result[i] {
+			t.Fatalf(`Error %d`, result[i])
+		}
+	}
+}
+
+func TestCountingSort_OneValue(t *testing.T) {
+	want := []int{0}
+	x := []int{0}
+	result := CountingSort(x)
+	for i, v := range want {
+		if v != result[i] {
+			t.Fatalf(`Error %d`, result[i])
+		}
+	}
+}
+
+func TestCountingSort_EmptyArray(t *testing.T) {
+	want := []int{}
+	x := []int{}
+	result := CountingSort(x)
+	for i, v := range want {
+		if v != result[i] {
+			t.Fatalf(`Error %d`, result[i])
+		}
+	}
 }
