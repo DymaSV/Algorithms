af571853c5727540a0fbd421bdff2275ed2b2480
DymaSv <dymasvyrydonov@gmail.com>
2021-10-10 13:36:23 -0400

    Counting sort for negative values

--- countingsort/countingsort.go
+++ countingsort/countingsort.go
@@ -1,34 +1,42 @@
 package countingsort
 
+import "math"
+
 func CountingSort(arr []int) []int {
 	return getSortedArray(prefixSum(getMap(arr)), arr)
 }
 
-func getMap(arr []int) (map[int]int, int) {
+func getMap(arr []int) (map[int]int, int, int) {
 	var arrCount = make(map[int]int)
-	max := 0
+	max := math.MinInt32
+	min := math.MaxInt32
 	for _, v := range arr {
 		if v > max {
 			max = v
 		}
+		if v < min {
+			min = v
+		}
 		arrCount[v] += 1
 	}
-	return arrCount, max
+	return arrCount, max, min
 }
 
-func prefixSum(m map[int]int, max int) map[int]int {
+func prefixSum(m map[int]int, max int, min int) map[int]int {
 	var arrCount = make(map[int]int)
-	for i := 0; i <= max; i++ {
-		if i != 0 {
+	for i := min; i <= max; i++ {
+		if i != min {
 			arrCount[i] = arrCount[i-1] + m[i]
 		} else {
 			arrCount[i] = m[i]
 		}
 	}
-	for i := max; i >= 0; i-- {
+
+	// Shift to the right
+	for i := max; i >= min; i-- {
 		arrCount[i] = arrCount[i-1]
 	}
-	arrCount[0] = 0
+	arrCount[min] = 0
 	return arrCount
 }
 
--- countingsort/countingsort_test.go
+++ countingsort/countingsort_test.go
@@ -6,8 +6,8 @@ import (
 
 // TestCountingSort calls countingsort.CountingSort.
 func TestCountingSort_WithDuplicates(t *testing.T) {
-	want := []int{0, 1, 2, 3, 3, 5, 5}
-	x := []int{3, 5, 0, 1, 5, 2, 3}
+	want := []int{-1, 0, 2, 3, 3, 5, 5}
+	x := []int{3, 5, 0, -1, 5, 2, 3}
 	result := CountingSort(x)
 	for i, v := range want {
 		if v != result[i] {
@@ -17,8 +17,8 @@ func TestCountingSort_WithDuplicates(t *testing.T) {
 }
 
 func TestCountingSort_WithOutDuplicates(t *testing.T) {
-	want := []int{0, 1, 2, 3, 5, 12, 16}
-	x := []int{3, 5, 0, 1, 12, 2, 16}
+	want := []int{-5, -3, 0, 1, 2, 12, 16}
+	x := []int{0, 1, 12, 2, 16, -3, -5}
 	result := CountingSort(x)
 	for i, v := range want {
 		if v != result[i] {
